b7876c9193061b42662b16e9ee96f41a0e261060
Elliot Braud <87452030+ESB24@users.noreply.github.com>
2024-10-11 13:19:32 +0200

    Update MOSjump.jl

--- src/MOSjump.jl
+++ src/MOSjump.jl
@@ -22,54 +22,128 @@ function set2SPA(C::Array{Int,2}, A::Array{Int,2}, varType::Symbol)
     return m2SPA
 end
 
-function set2SPA_2(m2SPA, C::Array{Int,2}, A::Array{Int,2})
+# fixed 0s
+function set2SPA_2(sols, C::Array{Int,2}, A::Array{Int,2})
     res = []
+    obj = []
 
-    n::Int64 = result_count(m2SPA)
-    relax = []
-    for i=1:n
-        push!(relax, value.(m2SPA[:x]; result = i))
-    end
+    n::Int64 = length(sols)
+
+    println("n = $n")
 
     for s = 1:n
-        used::Vector{Int64} = findall(x -> x == 1, relax[s])
-        frac::Vector{Int64} = findall(x -> 0 < x < 1, relax[s])
+        not_used::Vector{Int64} = findall(x -> x == 0, sols[s])
+        used::Vector{Int64} = findall(x -> x == 1, sols[s])
+        frac::Vector{Int64} = findall(x -> 0 < x < 1, sols[s])
+
+        frac = [frac; used]
 
         if length(frac) > 0
 
             nbctr, nbvar = size(A)
             m2SPA_2 = Model()
             
-            @variable(m2SPA_2, x[used], Bin)
+            @variable(m2SPA_2, x[1:nbvar], Bin)
             
-            @expression(m2SPA_2, obj1, sum((C[1,i])*x[i] for i in frac))
-            @expression(m2SPA_2, obj2, sum((C[2,i])*x[i] for i in frac))
+            @expression(m2SPA_2, obj1, sum((C[1,i])*x[i] for i in 1:nbvar))
+            @expression(m2SPA_2, obj2, sum((C[2,i])*x[i] for i in 1:nbvar))
             @objective(m2SPA_2, Min, [obj1, obj2])
+            @constraint(m2SPA_2, [i=1:nbctr],(sum((x[j]*A[i,j]) for j in 1:nbvar)) == 1) 
 
-            for i=1:nbctr
-                if !(sum((x[j]*A[i,j]) for j in 1:nbvar)) == 1
-                    @constraint(m2SPA_2, (sum((x[j]*A[i,j]) for j in frac)) == 1)
-                end
+            for i in not_used
+                @constraint(m2SPA_2, x[i] == 0)
             end
 
+            set_silent(m2SPA_2)
+            set_optimizer(m2SPA_2, () -> MOA.Optimizer(Gurobi.Optimizer))
+            set_optimizer_attribute(m2SPA_2, MOA.Algorithm(), MOA.Dichotomy())
+            set_optimizer_attribute(m2SPA_2, MOA.SolutionLimit(), 10)
+
             optimize!(m2SPA_2)
+
+            nb_sol = result_count(m2SPA_2)
             
-            for i=1:result_count(m2SPA_2)
+            println("STATUS = $(termination_status(m2SPA_2)), sol count = $(nb_sol)")
+            
+            for i=1:nb_sol
                 tmp = zeros(Int64, nbvar)
                 for (k, e) in enumerate(value.(m2SPA_2[:x]; result = i))
                     if e >= 0.7
                         tmp[k] = 1
                     end
                 end
-                for e in used
-                    tmp[e] = 1
-                end
+                # for e in used
+                #     tmp[e] = 1
+                # end
                 push!(res, tmp)
+                push!(obj, (sum([tmp[i] * C[1, i] for i=1:nbvar]), sum([tmp[i] * C[2, i] for i=1:nbvar])))
             end
         end
     end
 
-    return res
+    return res, obj
+end
+
+# penality for zros that changes to ones and one that changes to zeros
+function set2SPA_3(sols, C::Array{Int,2}, A::Array{Int,2}, env::Gurobi.Env = Gurobi.Env())
+    res = []
+    obj = []
+
+    _, n::Int64 = size(sols)
+
+    # println("n = $n")
+
+    for s = 1:n
+        not_used::Vector{Int64} = findall(x -> x == 0, sols[:, s])
+        used::Vector{Int64} = findall(x -> x == 1, sols[:, s])
+        frac::Vector{Int64} = findall(x -> 0 < x < 1, sols[:, s])
+
+        penalized::Vector{Int64} = [not_used; used]
+
+        frac = [frac; used]
+
+        if length(frac) > 0
+
+            nbctr, nbvar = size(A)
+            m2SPA_2 = Model(() -> Gurobi.Optimizer(env))
+            
+            @variable(m2SPA_2, x[1:nbvar], Bin)
+            @variable(m2SPA_2, y[1:nbvar], Bin)
+            
+            @objective(m2SPA_2, Min, sum(y))
+
+            @constraint(m2SPA_2, [i=1:nbctr],(sum((x[j]*A[i,j]) for j in 1:nbvar)) == 1) 
+
+            for i in not_used
+                @constraint(m2SPA_2, x[i] ≤ y[i])
+            end
+
+            for i in used
+                @constraint(m2SPA_2, x[i] ≥ 1 - y[i])
+            end
+
+            set_silent(m2SPA_2)
+
+            optimize!(m2SPA_2)
+
+            nb_sol = result_count(m2SPA_2)
+            
+            println("    → resolution status = $(termination_status(m2SPA_2)), feasible sol count = $(nb_sol)")
+            
+            for i=1:nb_sol
+                tmp = zeros(Int64, nbvar)
+                for (k, e) in enumerate(value.(m2SPA_2[:x]; result = i))
+                    if e >= 0.7
+                        tmp[k] = 1
+                    end
+                end
+                push!(res, tmp)
+                push!(obj, (sum([tmp[i] * C[1, i] for i=1:nbvar]), sum([tmp[i] * C[2, i] for i=1:nbvar])))
+            end
+        end
+    end
+
+    return res, obj
 end
 
 # ==============================================================================
